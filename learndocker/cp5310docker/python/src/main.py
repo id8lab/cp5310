cab730646d8957689259afbb5ac415f72e0d4cd2
Michael Hansen <michaelmotet.hansen@jcu.edu.au>
2019-08-30 17:16:54 +0800

    added flask

--- learndocker/cp5310docker/python/src/main.py
+++ learndocker/cp5310docker/python/src/main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "CP5310 python with Flask"
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     # Only for debugging while developing
-    app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(host='0.0.0.0', debug=True, port=8080)
